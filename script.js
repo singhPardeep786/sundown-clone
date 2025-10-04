6ed1410cce30da96976208d070d14740d7d20167
singhPardeep786 <pardeep90191170@gmail.com>
2025-10-04 10:24:16 +0530

    changes

--- script.js
+++ script.js
@@ -10,16 +10,17 @@ function page4Animation() {
     elemC.addEventListener("mouseenter", function () {
         fixed.style.display = "block"
     })
-    elemC.addEventListener("mouseleave", function () {
-        fixed.style.display = "none"
-    })
-
+    
     var elems = document.querySelectorAll(".elem")
     elems.forEach(function (e) {
         e.addEventListener("mouseenter", function () {
             var image = e.getAttribute("data-image")
             fixed.style.backgroundImage = `url(${image})`
         })
+    })
+
+    elemC.addEventListener("mouseleave", function () {
+      fixed.style.display = "none"
     })
 }
 
